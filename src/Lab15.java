57772a35867499328bcd874a4e2135a989e916ce
Jakub Adrian Niemiec <janiemiec20@gmail.com>
2017-03-30 21:12:36 +0100

    Need to fix distance array

--- src/Lab15.java
+++ src/Lab15.java
@@ -7,6 +7,8 @@
  * Student ID: 1500408
  */
 
+import java.util.List;
+
 /**
  * SA, RRHC, SHC, RMHC
  */
@@ -22,14 +24,44 @@ public class Lab15 {
 		int numberOfIterations = 0;
 		Utilities.LoadDataFile(48);
 
+		//Generate the first base tour
+		List<Integer> tour = Utilities.PopulateCities(matrixSize);
+		System.out.println(tour);
+		//TODO Distance Array not loading
+		System.out.println(distanceArray);
+		RMHC(tour, numberOfIterations);
+
 
 	}
 
-	private static void RMHC(int numberOfIterations) {
+	private static List<Integer> RMHC(List<Integer> tour, int numberOfIterations) {
+		List<Integer> oldTour, newTour, currentTour;
+		double oldFitness, newFitness, currentFitness;
+
 		//TODO Get the starting fitness of the current tour
-		//TODO Make a small change
-		//TODO Get Fitness of new tour
-		//TODO Compare fitnesses
-		//TODO choose next values
+		currentFitness = Utilities.ScoreTour(tour);
+		currentTour = tour;
+
+		System.out.println("=== Computing RMHC... Quiet Please ===");
+
+		for (int i = 1; i <= numberOfIterations; i++) {
+			//Save old values before making any changes
+			oldFitness = currentFitness;
+			oldTour = currentTour;
+
+			//TODO Make a small change
+			currentTour = Utilities.Swap(tour);
+			//TODO Get Fitness of new tour
+			currentFitness = Utilities.ScoreTour(currentTour);
+			//TODO Compare fitnesses
+			//We want to get the lowest possible tour length
+			if (currentFitness >= oldFitness) {
+				currentFitness = oldFitness;
+				currentTour = oldTour;
+			}
+			//TODO choose next values
+		}
+		System.out.println("Fitness: " + currentFitness);
+		return currentTour;
 	}
 }
