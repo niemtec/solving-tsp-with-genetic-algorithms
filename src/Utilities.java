8508df92dacefb93f78080a280db97e7b32d75d0
Jakub Adrian Niemiec <mail.niemtec@gmail.com>
2017-03-30 11:29:00 +0100

    Added the Swap function

--- src/Utilities.java
+++ src/Utilities.java
@@ -45,4 +45,79 @@ public class Utilities {
         System.out.println("Shuffled List: " + tour);
         return tour;
     }
+
+    /**
+     * Fitness Function for the Travelling Salesman Problem
+     *
+     * @param numberOfCities number of cities to visit within the tour
+     * @param tour           a list of integers of size N (tour taken by the salesman)
+     * @param matrix         an NxN matrix containing the distances between each city
+     * @return the tour length s (the fitness, lower is better)
+     */
+    private static double FitnessFunction(int numberOfCities, int[] tour, double[][] matrix) {
+        double s = 0.0;
+        for (int i = 1; i <= numberOfCities - 1; i++) {
+            int a = tour[i];
+            int b = tour[i + 1];
+            s = s + GetDistance(a, b);
+        }
+
+        int endCity = tour[numberOfCities];
+        int startCity = tour[1];
+        s = s + GetDistance(endCity, startCity);
+
+        return s;
+    }
+
+    /**
+     * Calculates the distance between city A and B
+     *
+     * @param a city A
+     * @param b city B
+     * @return distance between city A and B
+     */
+    private static double GetDistance(int a, int b) {
+        //Remember: Java is ROW major, this means that ROWs come first
+        double distance = Lab15.distanceArray[a][b];
+        return distance;
+    }
+
+    /**
+     * Prints array as a matrix format
+     *
+     * @param array input array to be printed
+     */
+    private static void PrintArray(double[][] array) {
+        int yLength = array[0].length;
+        int xLength = array[1].length;
+        int y, x;
+
+        try {
+            for (y = 0; y < yLength; y++) {
+                for (x = 0; x < xLength; x++) {
+                    System.out.print(array[y][x] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("==============================");
+        } catch (ArrayIndexOutOfBoundsException a) {
+            System.out.println();
+            System.out.println("WARNING: INCORRECT MATRIX ENTERED. INDEX OUT OF BOUNDS.");
+            System.out.println();
+        }
+    }
+
+    public static List<Integer> Swap(List<Integer> tour) {
+        int i = 0, j = 0;
+        while (i == j) {
+            i = UI(1, tour.size());
+            j = UI(1, tour.size());
+        }
+
+        int temp = tour.get(i);
+        tour.set(i, tour.get(i));
+        tour.set(j, temp);
+
+        return tour;
+    }
 }
